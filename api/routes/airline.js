8d186c89ec03efb5a9081b846acdd8c3bdfde45f
Edlaine Zamora <ezamora@thoughtworks.com>
2017-06-27 22:25:53 -0400

    Changed routes

--- api/routes/airline.js
+++ api/routes/airline.js
@@ -4,22 +4,22 @@ module.exports = function(app) {
   var flight = require('../controllers/flight');
   var passenger = require('../controllers/passenger');
 
-  app.route('/flight')
-    .get(flight.list_all_flight)
-    .post(flight.create_a_flight);
+  app.route('/flights')
+    .get(flight.list_all_flight)     //Consulta todos os vôos
+    .post(flight.create_a_flight);   //Cria um vôo
 
-  app.route('/flight/:flightId')
-    .get(flight.read_a_flight)
-    .put(flight.update_a_flight)
-    .delete(flight.delete_a_flight);
+  app.route('/flights/:flightId')     //Rodas passando o ID do vôo
+    .get(flight.read_a_flight)       //Consulta o vôo
+    .put(flight.update_a_flight)     //Atualiza o vôo
+    .delete(flight.delete_a_flight); //Deleta o vôo
 
-  app.route('/passenger')
-    .get(passenger.list_all_passenger)
-    .post(passenger.create_a_passenger);
+  app.route('/passengers')
+    .get(passenger.list_all_passenger)      //Consulta todos passageiros
+    .post(passenger.create_a_passenger);    //Cria um passageiro
 
-  app.route('/passenger/:passengerId')
-    .get(passenger.read_a_passenger)
-    .put(passenger.update_a_passenger)
-    .delete(passenger.delete_a_passenger);
+  app.route('/passengers/:passengerId')      //Rotas passando o ID do passageiro
+    .get(passenger.read_a_passenger)        //Consulta o passageiro
+    .put(passenger.update_a_passenger)      //Atualiza o passageiro
+    .delete(passenger.delete_a_passenger);  //Deleta o passageiro
 
 };
